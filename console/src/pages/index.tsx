bb2722e757eebe043619003e22ac723425a5801a
bachdx2812 <bachdx.hut@gmail.com>
2023-04-02 17:06:42 +0700

    add example for custom layout

--- console/src/pages/index.tsx
+++ console/src/pages/index.tsx
@@ -2,7 +2,16 @@ import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 
 const Page: NextPageWithLayout = () => {
-  return <p>Home page</p>
+  return <p>hello world</p>
 }
+
+// If you want to customize your layout of this poage
+// Page.getLayout = function getLayout(page: ReactElement) {
+//   return (
+//     <main>
+//       {page}
+//     </main>
+//   )
+// }
 
 export default Page
