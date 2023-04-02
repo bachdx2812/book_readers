e1d5691cf54ad1defdab388f152fcb0137a1ed60
bachdx2812 <bachdx.hut@gmail.com>
2023-04-02 16:58:00 +0700

    fix layouts

--- console/src/pages/_app.tsx
+++ console/src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import AuthedLayout from '@/layouts/AuthedLayout'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const getDefaultLayout = function(page: ReactElement) {
+  return (
+    <AuthedLayout>
+      { page }
+    </AuthedLayout>
+  )
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? getDefaultLayout;
+
+  return getLayout(<Component {...pageProps} />) 
 }
