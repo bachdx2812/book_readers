e2bffab8a5b583e057b84ea2e11e4574b024b7fa
anhnguyenminh <katnguyen500@gmail.com>
2023-05-26 11:54:48 +0700

    add lack file

--- front/src/layouts/AuthLayout.vue
+++ front/src/layouts/AuthLayout.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <div class="flex flex-col md:flex-row h-screen">
-      <div class="bg-white md:w-1/2 flex items-center justify-center">
+    <div
+      class="h-screen"
+      style="
+        background-image: linear-gradient(
+          to right bottom,
+          #7ad2ac,
+          #37bfaf,
+          #00abb5,
+          #0096ba
+        );
+      "
+    >
+      <div class="h-screen flex items-center justify-center">
         <slot></slot>
-      </div>
-      <div class="bg-cover bg-center md:w-1/2 h-screen"
-        style="background-image: url('https://cdn.discordapp.com/attachments/1094838801895018536/1103054130655797248/bachdx_flat_vector_illustration_depicting_a_group_of_people_rea_055bdfe7-7b34-481f-abf3-0ae1da3f6021.png')">
       </div>
     </div>
   </div>
